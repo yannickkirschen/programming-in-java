1ee166511ccbece0d631f307bca3cb28306c0205
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-03-31 19:33:52 +0200

    feat: initial commit
    Signed-off-by: Yannick Kirschen <yannickkirschen@protonmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programming in Java
+
+This is the source code of the lecture "Programming in Java" at DHBW Mannheim.
---
+++ src/main/java/sh/yannick/dhbw/hello_world/Main.java
@@ -0,0 +1,7 @@
+package sh.yannick.dhbw.hello_world;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
